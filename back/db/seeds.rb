37dcc471799f1ec1ad28c314dd3620cd64786d82
Riku Komiya <824rikukomiya@gmail.com>
2020-03-05 11:02:19 +0900

    lectureモデル追加 lecture,messageにリレーション追加

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+i = 1
+3.times do 
+  Lecture.create(name: "lecture#{i}", professor_name: "教授{¥#{i}", period: "1")
+  i += 1
+end
